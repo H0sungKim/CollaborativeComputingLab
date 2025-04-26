50a2ff679e73a3ba602b73d3044435d2aeaa3bde
Hosung.Kim <hosungkim5108@gmail.com>
2025-04-26 23:34:17 +0900

    WebSocket 기본 작동 확인 완료

---
+++ Backend/CollaborativeComputingLab/src/main/java/com/ccl/collaborativecomputinglab/CollaborativeComputingLabApplication.java
@@ -0,0 +1,13 @@
+package com.ccl.collaborativecomputinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeComputingLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeComputingLabApplication.class, args);
+	}
+
+}
---
+++ Backend/CollaborativeComputingLab/src/test/java/com/ccl/collaborativecomputinglab/CollaborativeComputingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccl.collaborativecomputinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeComputingLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
