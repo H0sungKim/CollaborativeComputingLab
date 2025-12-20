a4568989875e035515458cbe5725247fda950cfb
Hosung.Kim <hosungkim5108@gmail.com>
2025-12-20 15:24:34 +0900

    Add AppConfig

--- CollaborativeComputingLab/CollaborativeComputingLab/Application/SceneDelegate.swift
+++ CollaborativeComputingLab/CollaborativeComputingLab/Application/SceneDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    let diContainer: DIContainer = DefaultDIContainer(uri: Bundle.main.uri)
+    let diContainer: DIContainer = DefaultDIContainer(uri: AppConfig.uri)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
--- CollaborativeComputingLab/CollaborativeComputingLab/Util/AppConfig.swift
+++ CollaborativeComputingLab/CollaborativeComputingLab/Util/AppConfig.swift
@@ -1,17 +1,17 @@
 //
-//  Bundle+Ext.swift
+//  AppConfig.swift
 //  CollaborativeComputingLab
 //
-//  Created by 김호성 on 2025.03.07.
+//  Created by 김호성 on 2025.12.20.
 //
 
 import Foundation
 
-extension Bundle {
-    var uri: String {
-        guard let file = self.path(forResource: "Secret", ofType: "plist") else { fatalError("Secret.plist not found.") }
+enum AppConfig {
+    static let uri: String = {
+        guard let file = Bundle.main.path(forResource: "Secret", ofType: "plist") else { fatalError("Secret.plist not found.") }
         guard let resource = NSDictionary(contentsOfFile: file) else { fatalError("Secret.plist not found.") }
         guard let key = resource["uri"] as? String else { fatalError("uri not found.") }
         return key
-    }
+    }()
 }
